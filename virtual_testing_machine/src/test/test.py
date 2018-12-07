0d960d1b7c6970e39c22a4b5087e27ef2dce8d0b
TobKed <tobiaszkedzierski@gmail.com>
2018-12-07 21:56:04 +0100

    update test

--- virtual_testing_machine/src/test/test.py
+++ virtual_testing_machine/src/test/test.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import run
 from time import time
+from unittest.mock import MagicMock
 
 
 class TestDirsFiles(unittest.TestCase):
@@ -18,7 +19,16 @@ class TestRunTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.test_case_path = os.path.join(run.TEST_CASES_DIR, "01_feature_lamp", "01_01_test_lamp.py")
-        assert os.path.exists(cls.test_case_path)
+
+    def test_test_case_path(self):
+        self.assertTrue(os.path.exists(self.test_case_path))
+
+    @unittest.mock.patch("builtins.print")
+    def test_print_current_test_status(self, mocked_print):
+        test_string = "test status"
+        run.TestData.state = test_string
+        run.TestData.print_current_test_status()
+        mocked_print.assert_called_once_with('Current test status:', test_string)
 
     def test_run_test_case(self):
         time_start = time()
