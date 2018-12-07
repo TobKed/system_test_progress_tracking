38bfd7dcdad424c92019dfc19e024e037c396e23
TobKed <tobiaszkedzierski@gmail.com>
2018-12-07 07:15:10 +0100

    update test.py and .travis.yml

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
 import unittest
+import coverage
 
 
-virtual_testing_machine_tests = unittest.TestLoader().discover('virtual_testing_machine/src')
-unittest.TextTestRunner(verbosity=2).run(virtual_testing_machine_tests)
+COV = coverage.coverage(
+    omit=[
+        'test.py',
+        'virtual_testing_machine/src/test/*',
+    ]
+)
+COV.start()
+
+
+def cov():
+    """Runs the unit tests with coverage."""
+    virtual_testing_machine_tests = unittest.TestLoader().discover('virtual_testing_machine/src')
+    result = unittest.TextTestRunner(verbosity=2).run(virtual_testing_machine_tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        return 0
+    return 1
+
+
+if __name__ == '__main__':
+    cov()
