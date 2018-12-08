b453b418c6d8d886e2c44af1cc03bedc1ca3964c
TobKed <tobiaszkedzierski@gmail.com>
2018-12-08 19:59:46 +0100

    create classes to generate dry run JSON

--- virtual_testing_machine/src/test_runner/models.py
+++ virtual_testing_machine/src/test_runner/models.py
@@ -1,5 +1,41 @@
+from .settings import MACHINE_NAME
+import json
+from datetime import datetime
+
+
 class RunData:
     dry_run = False
+
+
+class DryRunData:
+    def __init__(self):
+        self.machine_name = MACHINE_NAME
+        self.time_stamp = None
+        self.master_scenario = None
+
+    def toJSON(self):
+        self.time_stamp = datetime.now()
+        return json.dumps(self, default=lambda o: vars(o), sort_keys=True, indent=4)
+
+
+class TestCaseData:
+    def __init__(self, file_path, file_name, script):
+        self.file_path = file_path
+        self.file_name = file_name
+        self.script = script
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: vars(o), sort_keys=True, indent=4)
+
+
+class MasterScenarioData(TestCaseData):
+    def __init__(self, file_path, file_name, script, tests=None):
+        self.tests = tests if tests else []
+        super().__init__(file_path, file_name, script)
+
+    def toJSON(self):
+        self.tests = [test.toJson() for test in self.tests] if self.tests else None
+        return json.dumps(self, default=lambda o: vars(o), sort_keys=True, indent=4)
 
 
 class TestData:
