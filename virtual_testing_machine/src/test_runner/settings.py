347de3c61480e759ff4e3cf3d5540da1a1799529
TobKed <tobiaszkedzierski@gmail.com>
2018-12-13 22:09:17 +0100

    add endpoints to settings

--- virtual_testing_machine/src/test_runner/settings.py
+++ virtual_testing_machine/src/test_runner/settings.py
@@ -7,3 +7,9 @@ BASE_DIR        = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEST_CASES_DIR  = os.path.join(BASE_DIR, "test_cases")
 SCENARIOS_DIR   = os.path.join(TEST_CASES_DIR, "scenarios")
 SCENARIO_MASTER = os.path.join(SCENARIOS_DIR, "scenario_master.py")
+
+ENDPOINT_BASE           = "localhost:8000/test_machine_input/"
+ENDPOINT_DRY_RUN        = ENDPOINT_BASE + "dry_run/"
+ENDPOINT_RUN_START      = ENDPOINT_BASE + "run_start/"
+ENDPOINT_RUN_STOP       = ENDPOINT_BASE + "run_stop/"
+ENDPOINT_RUN_EXCEPTION  = ENDPOINT_BASE + "run_exception/"
