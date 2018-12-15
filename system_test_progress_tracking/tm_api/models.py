baf949123ea450b479cc6e3374f7168fc9f7c68b
TobKed <tobiaszkedzierski@gmail.com>
2018-12-15 10:12:37 +0100

    add base models

--- system_test_progress_tracking/tm_api/models.py
+++ system_test_progress_tracking/tm_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseScript(models.Model):
+    file_name       = models.CharField(max_length=256)
+    file_path       = models.CharField(max_length=1024)
+    script          = models.TextField()
+
+
+class MasterScenario(BaseScript):
+    machine_name    = models.CharField(max_length=256)
+    time_stamp      = models.DateTimeField(null=True, blank=True)
+
+
+class Scenario(models.Model):
+    master_scenario = models.ForeignKey(MasterScenario, on_delete=models.CASCADE, related_name="scenarios")
+
+
+class Test(BaseScript):
+    scenario        = models.ForeignKey(Scenario, on_delete=models.CASCADE, related_name="tests")
