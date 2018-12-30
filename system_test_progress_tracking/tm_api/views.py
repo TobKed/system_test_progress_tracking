7f55bc97cd2fd072785e5a02f44a66dc2d9e06a7
TobKed <tobiaszkedzierski@gmail.com>
2018-12-30 12:59:34 +0100

    add timestamps to test start/stop views

--- system_test_progress_tracking/tm_api/views.py
+++ system_test_progress_tracking/tm_api/views.py
@@ -20,6 +20,7 @@ class TestStartView(APIView):
         serializer = TestStartSerializer(data=request.data)
         if serializer.is_valid():
             machine_name = serializer.validated_data.get("machine_name")
+            timestamp_start = serializer.validated_data.get("timestamp_start")
             file_name = serializer.validated_data.get("file_name")
             file_path = serializer.validated_data.get("file_path")
             machine = Machine.objects.get(machine_name__iexact=machine_name)
@@ -27,6 +28,7 @@ class TestStartView(APIView):
                                                    status=WAITING).first() if machine else None
             if first_waiting_test:
                 first_waiting_test.status = "running"
+                first_waiting_test.timestamp_start = timestamp_start
                 first_waiting_test.save()
                 return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -37,6 +39,7 @@ class TestStopView(APIView):
         serializer = TestStopSerializer(data=request.data)
         if serializer.is_valid():
             machine_name= serializer.validated_data.get("machine_name")
+            timestamp_stop = serializer.validated_data.get("timestamp_stop")
             status_ = serializer.validated_data.get("status")
             file_name = serializer.validated_data.get("file_name")
             file_path = serializer.validated_data.get("file_path")
@@ -45,6 +48,7 @@ class TestStopView(APIView):
                                                    status=RUNNING).first() if machine else None
             if first_running_test:
                 first_running_test.status = status_
+                first_running_test.timestamp_stop = timestamp_stop
                 first_running_test.save()
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
