64f196d9ae0c4fe43ee821ee860fc59f117cb247
TobKed <tobiaszkedzierski@gmail.com>
2019-01-02 22:53:14 +0100

    update consumers (working channnels)

--- system_test_progress_tracking/progress_tracking/consumers.py
+++ system_test_progress_tracking/progress_tracking/consumers.py
@@ -22,25 +22,34 @@ class MachineConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
-    # Receive message from WebSocket
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        machine_data = text_data_json['machineData']
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.machine_group_name,
-            {
-                'type': 'machineData',
-                'machineData': machine_data
-            }
-        )
+    # # Receive message from WebSocket
+    # async def receive(self, text_data):
+    #     text_data_json = json.loads(text_data)
+    #     machine_data = text_data_json['machine_data']
+    #
+    #     # Send message to room group
+    #     await self.channel_layer.group_send(
+    #         self.machine_group_name,
+    #         {
+    #             'type': 'machine_data',
+    #             'machine_data': machine_data
+    #         }
+    #     )
 
     # Receive message from room group
-    async def machine_data_send(self, event):
-        machine_data = event['machineData']
+    async def machine_data(self, event):
+        machine_data = event['machine_data']
         print(machine_data)
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'machineData': machine_data
+            'machine_data': machine_data
         }))
+
+"""
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+channel_layer = get_channel_layer()
+
+machine_id = 1
+async_to_sync(channel_layer.group_send)(f"machine_{machine_id}", {"type": "machine_data", "machine_data": "Hello there!", })
+"""
