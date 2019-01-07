682badd58f214c010262671df5370f0855bc78e9
TobKed <tobiaszkedzierski@gmail.com>
2019-01-07 19:13:22 +0100

    refactoring

--- system_test_progress_tracking/progress_tracking/consumers.py
+++ system_test_progress_tracking/progress_tracking/consumers.py
@@ -7,7 +7,7 @@ class MachineConsumer(AsyncWebsocketConsumer):
         self.machine_id = self.scope['url_route']['kwargs']['pk']
         self.machine_group_name = 'machine_%s' % self.machine_id
 
-        # Join room group
+        # Join machine group
         await self.channel_layer.group_add(
             self.machine_group_name,
             self.channel_name
@@ -25,7 +25,6 @@ class MachineConsumer(AsyncWebsocketConsumer):
     # Receive message from room group
     async def machine_data(self, event):
         machine_data = event['machine_data']
-        print(machine_data)
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'machine_data': machine_data
