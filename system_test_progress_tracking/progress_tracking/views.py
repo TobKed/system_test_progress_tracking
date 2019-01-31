78bce50448fcd43d92ee2fa7996251c23e6bf2c1
TobKed <tobiaszkedzierski@gmail.com>
2019-01-31 18:30:28 +0100

    fix saving page size to cookie from get method

--- system_test_progress_tracking/progress_tracking/views.py
+++ system_test_progress_tracking/progress_tracking/views.py
@@ -17,15 +17,19 @@ def home(request):
 class MachineListView(View):
     template_name = "progress_tracking/machine_list.html"
     paginate_by = CustomListPageSizePagination.page_size
+    cookie_name = "machineListPageSize"
 
     def get(self, request):
-        paginate_by = request.COOKIES.get("machineListPageSize", self.paginate_by)
+        paginate_by_cookie = request.COOKIES.get(self.cookie_name, self.paginate_by)
+        paginate_by = self.request.GET.get("page_size", paginate_by_cookie)
         params = {
             "count": Machine.objects.all().count(),
             "page_number": self.request.GET.get("page", "1"),
             "page_size": self.request.GET.get("page_size", paginate_by),
         }
-        return render(request, self.template_name, params)
+        response = render(request, self.template_name, params)
+        response.set_cookie(key=self.cookie_name, value=paginate_by)
+        return response
 
 
 class MachineDetailView(DetailView):
@@ -33,9 +37,19 @@ class MachineDetailView(DetailView):
     template_name = "progress_tracking/machine_detail.html"
     context_object_name = 'machine'
     paginate_by = CustomListPageSizePagination.page_size
+    cookie_name = "machineDetailPageSize"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        page_size = context.get("page_size")
+        response = self.render_to_response(context)
+        response.set_cookie(key=self.cookie_name, value=page_size)
+        return response
 
     def get_context_data(self, *args, **kwargs):
-        paginate_by = self.request.COOKIES.get("machineDetailPageSize", self.paginate_by)
+        paginate_by_cookie = self.request.COOKIES.get(self.cookie_name, self.paginate_by)
+        paginate_by = self.request.GET.get("page_size", paginate_by_cookie)
         context_data = super().get_context_data(*args, **kwargs)
         dry_run_datas = self.object.dry_run_datas.all()
         paginator = Paginator(dry_run_datas, self.paginate_by)
